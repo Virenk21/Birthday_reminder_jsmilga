d99079981fefe0c0bef86d468771dd6ecc9242b7
Virenk21 <viren.d222111@gmail.com>
2022-03-30 09:12:26 +0530

    updated data and list files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 import List from './List';
 
-
 function App() {
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className='container'>
-        <h3>0 birthdays Today</h3>
-        <List />
-        <button onClick={() => console.log('u clicked me')}>clear all</button>
+        <h3> {people.length} birthdays Today</h3>
+        <List people={people} />
+         <button onClick={() => console.log('u clicked me')}>clear all</button>
       </section>
     </main>
   );
--- src/List.js
+++ src/List.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const List = () => {
+const List = ({people}) => {
   return (
-    <div>List</div>
-  )
+    <>
+     {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+         <article key={id} className="person">
+           <img src={image} alt={name} />
+           <div>
+             <h4>{name}</h4>
+             <p>{age} years</p>
+           </div>
+         </article>
+        );
+      })};
+        </>
+  );
 }
 
 export default List
