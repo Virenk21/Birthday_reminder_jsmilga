039c7f6b3b94e578aa39cb222430ffaef6ca4960
Virenk21 <viren.d222111@gmail.com>
2022-03-30 08:34:54 +0530

    updated files and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import List from './List';
+
 
 function App() {
   return (
-    <>
-    Viren
-    </>
+    <main>
+      <section className='container'>
+        <h3>0 birthdays Today</h3>
+        <List />
+        <button onClick={() => console.log('u clicked me')}>clear all</button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
